b730644d952a45eeba95f0f45396bef4fc271628
ipromise2324 <168allen55@gmail.com>
2023-08-10 23:48:48 +0800

    feat: add mint nft test

--- tests/NftCollection.spec.ts
+++ tests/NftCollection.spec.ts
@@ -1,19 +1,19 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
 import { NftCollection } from '../wrappers/NftCollection';
 import '@ton-community/test-utils';
+import { NftItem } from '../wrappers/NftItem';
 
 describe('NftCollection', () => {
     let blockchain: Blockchain;
     let nftCollection: SandboxContract<NftCollection>;
-
+    let deployer: SandboxContract<TreasuryContract>;
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        const deployer = await blockchain.treasury('deployer');
         const content = "I Promise NFT Collection";
         nftCollection = blockchain.openContract(await NftCollection.fromInit(content));
 
-
+        deployer = await blockchain.treasury('deployer');
 
         const deployResult = await nftCollection.send(
             deployer.getSender(),
@@ -38,4 +38,26 @@ describe('NftCollection', () => {
         // the check is done inside beforeEach
         // blockchain and nftCollection are ready to use
     });
+
+    it("should mint nft", async()=>{
+        
+        const res = await nftCollection.send(deployer.getSender(), {
+            value: toNano("0.3")
+        }, 'Mint')
+
+        const nftItemAddress = await nftCollection.getGetNftAddressByIndex(0n);
+        const nftItem: SandboxContract<NftItem> = blockchain.openContract(NftItem.fromAddress(nftItemAddress!))
+
+        const buyer = await blockchain.treasury("buyer");
+        await nftItem.send(deployer.getSender(), {
+            value: toNano("0.2")
+        }, {
+            $$type: 'Transfer',
+            new_owner: buyer.address,
+            query_id: 0n
+        })
+
+        const nftItemData = await nftItem.getGetItemData();
+        expect(nftItemData.owner).toEqualAddress(buyer.address);
+    })
 });
